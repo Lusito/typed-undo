29f4428e522ff8ad87f107d1e3c6aee6c21b1a61
Lusito <core@roughael.net>
2017-12-03 18:58:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+![](https://lusito.github.io/typed-undomanager/typed_undomanager.png)
+
+[![License](https://img.shields.io/badge/License-CC0%20Public%20Domain-blue.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
+
+|Master|[![Build Status](https://travis-ci.org/Lusito/typed-undomanager.svg?branch=master)](https://travis-ci.org/Lusito/typed-undomanager)|[![Code Coverage](https://coveralls.io/repos/github/Lusito/typed-undomanager/badge.svg?branch=master)](https://coveralls.io/github/Lusito/typed-undomanager)|
+|---|---|---|
+|Develop|[![Build Status](https://travis-ci.org/Lusito/typed-undomanager.svg?branch=develop)](https://travis-ci.org/Lusito/typed-undomanager)|[![Code Coverage](https://coveralls.io/repos/github/Lusito/typed-undomanager/badge.svg?branch=develop)](https://coveralls.io/github/Lusito/typed-undomanager)|
+
+An UndoManager for TypeScript (and JavaScript). The basic idea is based on the UndoManager from Java.
+
+### Why Typed-UndoManager?
+
+- Easily add memory-efficient undo/redo functionality.
+  - You can merge and replace edits, just like in the Java UndoManager.
+- You can mark a save-point, to enable/disable your save-button.
+- Undo/Redo non-edit related changes, like focus change, without it being marked as needs-to-be-saved.
+- No dependencies
+- Automated [unit tests](https://travis-ci.org/Lusito/typed-undomanager)  with 100% [code coverage](https://coveralls.io/github/Lusito/typed-undomanager)
+- Liberal license: [zlib/libpng](https://github.com/Lusito/typed-undomanager/blob/master/LICENSE)
+- [Fully documented](https://lusito.github.io/typed-undomanager/index.html) using TypeDoc
+
+### Installation via NPM
+
+```npm install typed-undomanager --save```
+
+### Basics
+
+```typescript
+import { UndoManager, UndoableEdit } from "typed-undomanager";
+
+class MyUndoableEdit extends UndoableEdit {
+    private readonly oldValue: string;
+    private newValue: string;
+
+	public undo(): void {
+	}
+
+	public redo(): void {
+	}
+}
+
+// fixme: examples
+```
+
+### Report isssues
+
+Something not working quite as expected? Do you need a feature that has not been implemented yet? Check the [issue tracker](https://github.com/Lusito/typed-undomanager/issues) and add a new one if your problem is not already listed. Please try to provide a detailed description of your problem, including the steps to reproduce it.
+
+### Contribute
+
+Awesome! If you would like to contribute with a new feature or submit a bugfix, fork this repo and send a pull request. Please, make sure all the unit tests are passing before submitting and add new ones in case you introduced new features.
+
+### License
+
+Typed-UndoManager has been released under the [zlib/libpng](https://github.com/Lusito/typed-undomanager/blob/master/LICENSE) license, meaning you
+can use it free of charge, without strings attached in commercial and non-commercial projects. Credits are appreciated but not mandatory.
---
+++ test/UndoManagerTests.ts
@@ -0,0 +1,447 @@
+/**
+ * License: zlib/libpng
+ * @author Santo Pfingsten
+ * @see https://github.com/Lusito/typed-undomanager
+ */
+
+import { suite, test } from "mocha-typescript";
+import { assert } from "chai";
+import { UndoManager, UndoableEdit } from "../src/UndoManager";
+
+class UndoableSpy extends UndoableEdit {
+	public undoneCount = 0;
+	public redoneCount = 0;
+	public undo(): void {
+		this.undoneCount++;
+	}
+	public redo(): void {
+		this.redoneCount++;
+	}
+}
+
+class UndoableMergeSpy extends UndoableSpy {
+	public mergeCalls = 0;
+	public mergeCount = 0;
+	public merge(edit: UndoableEdit): boolean {
+		this.mergeCalls++;
+		if (edit instanceof UndoableMergeSpy) {
+			this.mergeCount++;
+			return true;
+		}
+		return false;
+	}
+}
+
+class UndoableInsignificantSpy extends UndoableSpy {
+	public isSignificant(): boolean {
+		return false;
+	}
+}
+
+class UndoableMerge extends UndoableSpy {
+	private readonly oldValue: number;
+	private newValue: number;
+	public constructor(oldValue: number, newValue: number) {
+		super();
+		this.oldValue = oldValue;
+		this.newValue = newValue;
+	}
+
+	public merge(edit: UndoableEdit): boolean {
+		if (edit instanceof UndoableMerge) {
+			this.newValue = edit.newValue;
+			return true;
+		}
+		return false;
+	}
+	public isSignificant(): boolean {
+		return this.newValue !== this.oldValue;
+	}
+}
+
+@suite export class UndoManagerTests {
+	@test add_undo_and_redo() {
+		const manager = new UndoManager();
+		const undoable = new UndoableSpy();
+		manager.add(undoable);
+		assert.equal(undoable.undoneCount, 0);
+		assert.equal(undoable.redoneCount, 0);
+		assert.isTrue(manager.canUndo());
+		assert.isFalse(manager.canRedo());
+		manager.undo();
+		assert.equal(undoable.undoneCount, 1);
+		assert.equal(undoable.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable.undoneCount, 1);
+		assert.equal(undoable.redoneCount, 1);
+	}
+
+	@test add_two_undo_and_redo() {
+		const manager = new UndoManager();
+		const undoable1 = new UndoableSpy();
+		const undoable2 = new UndoableSpy();
+		manager.add(undoable1);
+		manager.add(undoable2);
+		assert.isTrue(manager.canUndo());
+		assert.isFalse(manager.canRedo());
+		manager.undo();
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.isTrue(manager.canUndo());
+		assert.isTrue(manager.canRedo());
+		manager.undo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 1);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 1);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 1);
+	}
+
+	@test add_undo_and_redo_middle() {
+		const manager = new UndoManager();
+		const undoable1 = new UndoableSpy();
+		const undoable2 = new UndoableSpy();
+		const undoable3 = new UndoableSpy();
+		manager.add(undoable1);
+		manager.add(undoable2);
+		manager.undo();
+		manager.add(undoable3);
+		assert.isTrue(manager.canUndo());
+		assert.isFalse(manager.canRedo());
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 0);
+		assert.equal(undoable3.redoneCount, 0);
+		manager.undo();
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 0);
+		manager.undo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 1);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 1);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 1);
+	}
+
+	@test merge_undo_redo() {
+		const manager = new UndoManager();
+		const undoable1 = new UndoableSpy();
+		const undoable2 = new UndoableMergeSpy();
+		const undoable3 = new UndoableMergeSpy();
+		const undoable4 = new UndoableSpy();
+		manager.add(undoable1);
+		manager.add(undoable2);
+		manager.add(undoable3);
+		manager.add(undoable4);
+		assert.equal(undoable2.mergeCalls, 2);
+		assert.equal(undoable2.mergeCount, 1);
+		assert.equal(undoable3.mergeCalls, 0);
+		manager.undo();
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 0);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 0);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 0);
+		manager.undo();
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 0);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 0);
+		manager.undo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 0);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 1);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 0);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 1);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 1);
+		assert.equal(undoable3.undoneCount, 0);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 1);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 1);
+		assert.equal(undoable3.undoneCount, 0);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 1);
+	}
+
+	@test limit() {
+		const manager = new UndoManager(2);
+		const undoable1 = new UndoableSpy();
+		const undoable2 = new UndoableSpy();
+		const undoable3 = new UndoableSpy();
+		assert.equal(manager.getLimit(), 2);
+		manager.add(undoable1);
+		manager.add(undoable2);
+		manager.add(undoable3);
+		assert.isTrue(manager.canUndo());
+		assert.isFalse(manager.canRedo());
+		manager.undo();
+		assert.isTrue(manager.canUndo());
+		assert.isTrue(manager.canRedo());
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 0);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 0);
+		manager.undo();
+		assert.isFalse(manager.canUndo());
+		assert.isTrue(manager.canRedo());
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 0);
+		manager.redo();
+		assert.isTrue(manager.canUndo());
+		assert.isTrue(manager.canRedo());
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 1);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 0);
+		manager.redo();
+		assert.isTrue(manager.canUndo());
+		assert.isFalse(manager.canRedo());
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 1);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 1);
+	}
+
+	//Fixme: set limits after adding undoables
+
+	@test errors() {
+		const manager = new UndoManager();
+		assert.throws(() => manager.undo());
+		assert.throws(() => manager.redo());
+		manager.add(new UndoableSpy());
+		assert.throws(() => manager.redo());
+		manager.undo();
+		assert.throws(() => manager.undo());
+	}
+
+	@test clear() {
+		const manager = new UndoManager();
+		manager.add(new UndoableSpy());
+		manager.add(new UndoableSpy());
+		manager.add(new UndoableSpy());
+		manager.clear();
+		assert.isFalse(manager.canUndo());
+		assert.isFalse(manager.canRedo());
+		assert.throws(() => manager.undo());
+		assert.throws(() => manager.redo());
+
+		// perform basic test afterwards to see if it still works.
+		const undoable = new UndoableSpy();
+		manager.add(undoable);
+		assert.equal(undoable.undoneCount, 0);
+		assert.equal(undoable.redoneCount, 0);
+		assert.isTrue(manager.canUndo());
+		assert.isFalse(manager.canRedo());
+		manager.undo();
+		assert.equal(undoable.undoneCount, 1);
+		assert.equal(undoable.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable.undoneCount, 1);
+		assert.equal(undoable.redoneCount, 1);
+	}
+
+	@test insignificant() {
+		const manager = new UndoManager();
+		const undoable1 = new UndoableSpy();
+		const undoable2 = new UndoableInsignificantSpy();
+		const undoable3 = new UndoableInsignificantSpy();
+		const undoable4 = new UndoableSpy();
+		manager.add(undoable1);
+		manager.add(undoable2);
+		manager.add(undoable3);
+		manager.add(undoable4);
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 0);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 0);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 0);
+		assert.equal(undoable4.redoneCount, 0);
+		manager.undo();
+		assert.equal(undoable1.undoneCount, 0);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 0);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 0);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 0);
+		manager.undo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 0);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 0);
+		assert.isFalse(manager.canUndo());
+		manager.redo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 1);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 0);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 0);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 0);
+		manager.redo();
+		assert.equal(undoable1.undoneCount, 1);
+		assert.equal(undoable1.redoneCount, 1);
+		assert.equal(undoable2.undoneCount, 1);
+		assert.equal(undoable2.redoneCount, 1);
+		assert.equal(undoable3.undoneCount, 1);
+		assert.equal(undoable3.redoneCount, 1);
+		assert.equal(undoable4.undoneCount, 1);
+		assert.equal(undoable4.redoneCount, 1);
+		assert.isFalse(manager.canRedo());
+	}
+
+	@test save_marker() {
+		const manager = new UndoManager();
+		assert.isFalse(manager.isModified());
+		const undoable1 = new UndoableSpy();
+		manager.add(undoable1);
+		assert.isTrue(manager.isModified());
+		manager.undo();
+		assert.isFalse(manager.isModified());
+		manager.redo();
+		assert.isTrue(manager.isModified());
+		manager.setUnmodified();
+		assert.isFalse(manager.isModified());
+		manager.undo();
+		assert.isTrue(manager.isModified());
+		manager.redo();
+		assert.isFalse(manager.isModified());
+	}
+
+	@test save_marker_insignificant() {
+		const manager = new UndoManager();
+		const undoable1 = new UndoableSpy();
+		const undoable2 = new UndoableInsignificantSpy();
+		const undoable3 = new UndoableInsignificantSpy();
+		const undoable4 = new UndoableSpy();
+		manager.add(undoable1);
+		manager.add(undoable2);
+		manager.add(undoable3);
+		manager.add(undoable4);
+
+		assert.isTrue(manager.isModified());
+		manager.setUnmodified();
+		assert.isFalse(manager.isModified());
+		manager.undo(); // 4 undone
+		assert.isTrue(manager.isModified());
+		manager.setUnmodified();
+		assert.isFalse(manager.isModified());
+		manager.undo(); // 3,2,1 undone
+		assert.isTrue(manager.isModified());
+		manager.redo(); // 1 redone
+		assert.isFalse(manager.isModified());
+		manager.redo(); // 2,3,4 redone
+		assert.isTrue(manager.isModified());
+		manager.setUnmodified();
+		manager.undo(); // 4 undone
+		manager.undo(); // 3,2,1 undone
+		assert.isTrue(manager.isModified());
+		manager.redo(); // 1 redone
+		assert.isTrue(manager.isModified());
+		manager.redo(); // 2,3,4 redone
+		assert.isFalse(manager.isModified());
+	}
+
+	@test save_marker_merged_insignificant() {
+		const manager = new UndoManager();
+		manager.add(new UndoableMerge(0, 1));
+		assert.isTrue(manager.isModified());
+		manager.add(new UndoableMerge(1, 0));
+		assert.isFalse(manager.isModified());
+	}
+
+	@test save_marker_saved_then_merged_insignificant() {
+		const manager = new UndoManager();
+		manager.add(new UndoableMerge(0, 1));
+		assert.isTrue(manager.isModified());
+		manager.setUnmodified();
+		manager.add(new UndoableMerge(1, 0));
+		assert.isTrue(manager.isModified());
+	}
+
+	//fixme: save marker scenario:
+	// do change (is significant), save, do change which merges,
+	// making current change insignificant => should say modified, but is unmodified
+
+	//Fixme: replace
+	//Fixme: listener
+}
